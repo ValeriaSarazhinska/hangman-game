59cc206e973e3448b72844c25f7769f6af0731a0
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-07-05 17:06:45 +0300

    create hangman-game

---
+++ src/Keyboard.tsx
@@ -0,0 +1,76 @@
+import styles from "./Keyboard.module.css";
+const KEYS = [
+  "а",
+  "б",
+  "в",
+  "г",
+  "ґ",
+  "д",
+  "е",
+  "є",
+  "ж",
+  "з",
+  "и",
+  "і",
+  "ї",
+  "й",
+  "к",
+  "л",
+  "м",
+  "н",
+  "о",
+  "п",
+  "р",
+  "с",
+  "т",
+  "у",
+  "ф",
+  "х",
+  "ц",
+  "ч",
+  "ш",
+  "щ",
+  "ь",
+  "ю",
+  "я",
+];
+
+type KeyboardProps = {
+  disabled?: boolean;
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit,minmax(65px,1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""}
+            ${isInactive ? styles.inactive : ""}`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
